09c9d820eadc2376141d0c31a3da7b9e9a68f795
cogcamp <codeofgenius.camp@gmail.com>
2024-12-27 06:41:28 +0000

    commit

--- Ema_2/main.js
+++ Ema_2/main.js
@@ -2,31 +2,61 @@ var mainScene = new Phaser.Scene("mainScene");
 
 mainScene.create = function() {
     // 初期設定を実行する
-    
+    this.config();
     
     // ボール作成
-    
+    this.createBall();
     
     // パドル作成
-    
+    this.createPaddle();
     
     // スペースキーのクリックでボール発射
-    
+    this.input.keyboard.on('keydown-SPACE', function(event) {
+        //ゲーム開始状態ならば
+        if (this.paddle.isStart) {
+            //ボール発射
+            this.ball.setVelocity(this.ballSpeedX, this.ballSpeedY);
+            this.paddle.isStart = false;
+        }
+    }, this);
     
     // ブロック作成
-    
+    this.createBlocks();
     
     // ライフのテキスト表示
+    this.lifeText = this.add.text(30, 20, 'ライフ:' + this.life, {
+        font: '20px Open Sans',
+        fill: '#000000'
+    });
     
 };
 
 mainScene.update = function() {
     // ボールがシーンの最下部に到達した
+    if(this.ball.y >= this.game.config.height - this.ball.width / 2) {
+        this.failToHit();
+    }
     
     
     // キーボードのカーソルオブジェクトを取得
     var cursors = this.input.keyboard.createCursorKeys();
     var x = 0;
+    //右カーソルをクリックすると
+    if(cursors.right.isDown){
+        x = this.paddle.x + this.paddleSpeed;
+        this.paddle.x = Phaser.Math.Clamp(x, 52, 748);
+    }
+    //左カーソルをクリックすると
+    if(cursors.left.isDown){
+        x = this.paddle.x - this.paddleSpeed;
+        this.paddle.x = Phaser.Math.Clamp(x, 52, 748);
+    }
+    
+    
+    //パドルの上にボールが乗っているなら
+    if(this.paddle.isStart){
+        this.ball.setPosition(this.paddle.x, 500);
+    }
     
 };
 
@@ -39,31 +69,71 @@ mainScene.config = function() {
     
     // ボール発射の加速度
     this.ballSpeedX = 0;
-    this.ballSpeedY = -300;
+    this.ballSpeedY = -300; 
     
     // ライフ
-    this.life = 3;
+    this.life = 1;
 };
 
 mainScene.createBall = function() {
     // ボール作成
-    
+    this.ball= this.physics.add.image(400, 500, 'ball1');
+    this.ball.setDisplaySize(22,22);
+    this.ball.setCollideWorldBounds(true);
+    this.ball.setBounce(1);
 };
 
 mainScene.createPaddle = function() {
      // パドル作成
-    
+ this.paddle = this.physics.add.image(400, 550, 'paddle1');   
+ this.paddle.setDisplaySize(110,20);
+ this.paddle.setImmovable();
+ this.paddle.isStart = true;
+ this.physics.add.collider(this.paddle, this.ball, this.hitPaddle, null, this);
+ 
 };
 
 mainScene.hitPaddle = function (paddle, ball) {
     // ボールにX方向の角度を設定
-    
+   var diff = 0;
+   if(ball.x< paddle.x){
+       //ボールがパドルの左側に衝突
+       diff=paddle.x - ball.x;
+       ball.setVelocityX(-10 * diff);
+   }else if (ball.x> paddle.x) {
+       //ボールがパドルの右側に衝突
+       diff = ball.x - paddle.x;
+       ball.setVelocityX(10 * diff);
+   }else {
+       //X方向の加速度はなし
+       ball.setVelocityX(0);
+   }
 };
 
 mainScene.createBlocks = function() {
-    // 横10列、縦6行並べる
+    // 横8列、縦6行並べる
+    //ブロックの色の配列
+    var blockColors = [ 'red1', 'green1', 'yellow1', 'silver1', 'blue1', 'purple1' ];
+   
+   
+   //物理エンジン対象固定オブジェクトグループ作成
+   this.blocks = this.physics.add.staticGroup();
+   
+   
+   //縦に6行
+   for(var i = 0; i < 6; i++ ){
+       //横に10列
+       for(var j = 0; j < 8; j++ ){
+           var color = blockColors[i];
+           var block = this.blocks.create(80 + j * 80, 80 + i * 40, color);
+           block.setOrigin(0,0);
+           block.setDisplaySize(80, 40);
+           block.refreshBody();
+       }
+   }
     
     
+    this.physics.add.collider(this.ball, this.blocks, this.hitBlock, null, this);
 };
 
 mainScene.hitBlock = function (ball, block) {
@@ -98,6 +168,11 @@ mainScene.failToHit =  function () {
     // ライフが0になると
     if(this.life <= 0) {
         // 0.5秒後にゲームオーバー
+            this.Text = this.add.text(350, 300, 'Game Over:' , {
+        font: '80px Open Sans',
+        fill: '#ff0000'
+    });
+
         this.time.addEvent({
             duration: 500,
             callback: this.gameOver,
@@ -109,7 +184,8 @@ mainScene.failToHit =  function () {
 
 mainScene.gameOver = function() {
     // ゲームオーバー
-    alert("ゲームオーバー");
+    console.log(this.Text);
+    alert("もう一度");
     // スタートシーンに移動
     this.scene.start("startScene");
 };
